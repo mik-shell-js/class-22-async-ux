280ec9180032af3b249004fcb2ab046ce40d265b
Michel Adelien <mha57@cornell.edu>
2025-04-16 15:21:59 +0000

    completed assignment 22

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -9,44 +9,62 @@ import Alert from './components/Alert';
 export default function Home() {
 
   const [spaceData, setSpaceData] = useState([]);
+  const [isLoadingData, setIsLoadingData] = useState(false);
+  const [dataError, setDataError] = useState();
+
 
   function loadSpaceData() {
+    setIsLoadingData(true);
+    setDataError(undefined);
+
     axios.get('/api/space')
       .then((response) => {
         setSpaceData(response.data);
       })
+      .catch((error) => {
+        setDataError(error);
+      })
+      .finally(() => {
+        setIsLoadingData(false);
+      })
   }
+
+  useEffect(() => {
+    loadSpaceData()
+  }, []);
 
   return (
     <main className="p-4 space-y-2">
       {/* error */}
-      {false &&
+      {dataError &&
         <Alert type='danger'>
           <div className="flex w-full items-center">
             <span className="flex-grow">There was a problem loading the data. Try reloading the data.</span>
-            <Button>
+            <Button onClick={loadSpaceData}>
               Reload
             </Button>
           </div>
         </Alert>}
 
       {/* loading */}
-      {false &&
+      {isLoadingData &&
         <div className="flex justify-center items-center h-[80vh]">
           <Spinner size="large" />
         </div>}
 
-      <div className="flex flex-wrap gap-4">
-        {spaceData.map((item) => (
-          <Card
-            key={item._id}
-            title={item.name}
-            imgUri={item.imageUri}
-          >
-            {item.description}
-          </Card>
-        ))}
-      </div>
+      {(!isLoadingData && !dataError) &&
+        <div className="flex flex-wrap gap-4">
+          {spaceData.map((item) => (
+            <Card
+              key={item._id}
+              title={item.name}
+              imgUri={item.imageUri}
+            >
+              {item.description}
+            </Card>
+          ))}
+        </div>
+      }
     </main>
   );
 }
